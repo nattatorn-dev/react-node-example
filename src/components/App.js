473398b1f635068b31821abad941f4af77a0f943
nattatorn.dev <nattatorn.dev@gmail.com>
2017-05-27 21:20:20 +0700

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import VideoPlayer from './VideoPlayer'
+
 import '../assets/stylesheets/base.scss'
 
 const App = ({ name }) => {
-  return (
-    <div>
-      <h1>Hello, {name}!</h1>
-      <VideoPlayer />
-    </div>
-  )
+  return <h1>Hello, {name}!</h1>
 }
 
 App.propTypes = {
