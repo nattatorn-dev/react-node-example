6d1f316ec8f64c082beae3327c6079b3474636fc
nattatorn.dev <nattatorn.dev@gmail.com>
2017-05-27 21:06:59 +0700

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-import '../assets/stylesheets/base.scss';
-
+import React from 'react'
+import PropTypes from 'prop-types'
+import VideoPlayer from './VideoPlayer'
+import '../assets/stylesheets/base.scss'
 
 const App = ({ name }) => {
   return (
-    <h1>Hello, {name}!</h1>
-  );
-};
+    <div>
+      <h1>Hello, {name}!</h1>
+      <VideoPlayer />
+    </div>
+  )
+}
 
 App.propTypes = {
-  name: PropTypes.string,
-};
+  name: PropTypes.string
+}
 
-export default App;
+export default App
